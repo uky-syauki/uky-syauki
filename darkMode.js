48b2def723c5f2982724f37006f7df3e33b4b264
ahmad-syauki <57953087+uky-syauki@users.noreply.github.com>
2022-12-15 00:49:47 +0800

    Update darkMode.js

--- darkMode.js
+++ darkMode.js
@@ -1,6 +1,7 @@
 function darkMode(){
 	document.getElementById('documentation').style.background = 'black';
 	document.getElementById('main').style.background = 'black';
+	let a = document.getElementsByTagName('font');
 	for (let isi in a.valueOf()){
 		a[isi].style.color = 'white';
 		if (isi == 767) break;
